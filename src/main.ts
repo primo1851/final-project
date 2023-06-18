c3c071fe87cf8a663b5305e98992982046fcf77b
primo1851 <luiza.lp@gec.inatel.br>
2023-06-18 14:40:37 -0300

    feat: add db feadback

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppDataSource } from './Database/dataSource';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const isInitialized: boolean = AppDataSource.isInitialized;
+  console.log(isInitialized);
 }
 bootstrap();
